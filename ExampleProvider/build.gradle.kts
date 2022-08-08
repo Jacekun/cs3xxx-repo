c2dea80290c8033bfaf0f7229fad14a579476e08
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-08-08 17:13:39 +0200

    update to recloudstream/gradle@c1a4db8

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,19 +1,22 @@
-version = "1.0.0"
+// use an integer for version numbers
+version = 1
 
 
-// All of these properties are optional, you can safely remove them
+cloudstream {
+    // All of these properties are optional, you can safely remove them
 
-description = "Lorem Ipsum"
+    description = "Lorem Ipsum"
+    authors = listOf("Cloudburst")
 
-/**
-* Status int as the following:
-* 0: Down
-* 1: Ok
-* 2: Slow
-* 3: Beta only
-* */
-status = 1
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
 
-// Set to true to get an 18+ symbol next to the plugin
-// adult = false // TODO: fix
-
+    // Set to true to get an 18+ symbol next to the plugin
+    isAdult = false // will be false if unspecified
+}
