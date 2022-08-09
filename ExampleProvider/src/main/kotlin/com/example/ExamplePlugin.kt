8863cdf57c83fa508c9c17bab7836233da8259b2
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-08-09 14:19:25 +0200

    update to recloudstream/cloudstream@bceeec8

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -2,13 +2,12 @@ package com.example
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import com.lagradost.cloudstream3.APIHolder
 import android.content.Context
 
 @CloudstreamPlugin
 class TestPlugin: Plugin() {
     override fun load(context: Context) {
-        // All providers should be added in this manner
-        APIHolder.allProviders.add(ExampleProvider())
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(ExampleProvider())
     }
 }
