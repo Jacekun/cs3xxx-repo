f25a4051081743967019eb4fa131feaa28f177ad
Red <25087116+AbdullahM0hamed@users.noreply.github.com>
2022-08-16 16:18:12 +0200

    A slight convenience
    Co-authored-by: Cloudburst <18114966+C10udburst@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,18 @@ rootProject.name = "CloudstreamPlugins"
 // This file sets what projects are included. Every time you add a new project, you must add it
 // to the includes below.
 
-// Plugins are included like this
-include(
-    "ExampleProvider"
-)
+val disabled = listOf()
+
+File(rootDir, ".").eachDir { dir ->
+    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
+        include(dir.name)
+    }
+}
+
+fun File.eachDir(block: (File) -> Unit) {
+    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
+}
+
+
+// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
+// include("PluginName")
