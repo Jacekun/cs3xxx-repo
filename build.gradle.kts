817f41ac157d9755cdd49e5264bd7786fb8a7c7d
Jace <54625750+Jacekun@users.noreply.github.com>
2022-08-21 07:29:31 +0800

    Updated gradle info

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,10 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "user/repo")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "Jacekun/cs3xxx-repo")
+
+        description = "For the coomers and degenerates"
+        authors = listOf("Jace")
     }
 
     android {
