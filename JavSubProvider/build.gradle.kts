635e6b03a656ee132b7d6e6c5ccf12dee11bd3c2
Jace <54625750+Jacekun@users.noreply.github.com>
2022-08-21 07:43:05 +0800

    Javsub - update icon url

--- JavSubProvider/build.gradle.kts
+++ JavSubProvider/build.gradle.kts
@@ -22,5 +22,5 @@ cloudstream {
     // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
     tvTypes = listOf("NSFW")
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=javsub.co&sz=%size%"
+    iconUrl = "https://javsub.co/wp-content/uploads/2021/09/jav-sub.png"
 }
