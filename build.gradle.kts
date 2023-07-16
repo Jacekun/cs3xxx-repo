8d30295490b7c77dc40dfccf9c6682799bad7996
Jace <54625750+Jacekun@users.noreply.github.com>
2023-07-16 16:37:35 +0800

    Update build.gradle.kts
    - Fixed Repo path

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/Jacekun/cs3xxx-repo")
+        setRepo("cloudstream", "cs3xxx-repo", "codeberg")
 
         description = "For the coomers and degenerates"
         authors = listOf("Jace")
