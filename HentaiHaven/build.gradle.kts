18df4701975bda5ca461525a8eec21a7a6edb720
KillerDogeEmpire <edison.beckett@gmail.com>
2024-02-12 17:30:50 -0800

    Fixes to HentaiHaven

--- HentaiHaven/build.gradle.kts
+++ HentaiHaven/build.gradle.kts
@@ -6,7 +6,7 @@ cloudstream {
     // All of these properties are optional, you can safely remove them
 
     description = ""
-    authors = listOf("Jace")
+    authors = listOf("Jace, KillerDogeEmpire")
 
     /**
     * Status int as the following:
